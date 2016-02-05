96f7f85651215c41c0d1ddb4249cd388cdf57a00
Or Yaniv <oryaniv@users.noreply.github.com>
2016-02-05 11:40:42 +0200

    Create AnimationRepository.js

---
+++ src/AnimationRepository.js
@@ -0,0 +1,150 @@
+var inAnimationsrep = [
+    { name: "normalsize", level: 2 /* Standard */ },
+    { name: "background-fade-in", level: 1 /* Basic */ },
+    { name: "rotatex-in", level: 2 /* Standard */ },
+    { name: "rotateY-in", level: 2 /* Standard */ },
+    //animate.css
+    { name: "animated bounceIn", level: 1 },
+    { name: "animated bounceInUp", level: 1 },
+    { name: "animated bounceInDown", level: 1 },
+    { name: "animated bounceInRight", level: 1 },
+    { name: "animated bounceInLeft", level: 1 },
+    { name: "animated slideInUp", level: 1 },
+    { name: "animated slideInDown", level: 1 },
+    { name: "animated slideInRight", level: 1 },
+    { name: "animated slideInLeft", level: 1 },
+    { name: "animated lightSpeedIn", level: 2 },
+    { name: "animated zoomInLeft", level: 2 },
+    { name: "animated zoomInRight", level: 2 },
+    { name: "animated zoomInUp", level: 2 },
+    { name: "animated zoomInDown", level: 2 },
+    { name: "animated zoomIn", level: 2 },
+    { name: "animated rollIn", level: 2 },
+    { name: "animated lightSpeedIn", level: 2 },
+    { name: "animated fadeIn", level: 2 },
+    { name: "animated fadeInDownBig", level: 2 },
+    { name: "animated fadeInLeftBig", level: 2 },
+    { name: "animated fadeInRightBig", level: 2 },
+    { name: "animated fadeInUpBig", level: 2 },
+    { name: "animated fadeInDownBig", level: 2 },
+    { name: "animated flipInX", level: 2 },
+    { name: "animated flipInY", level: 2 },
+    { name: "animated rotateInDownLeft", level: 2 },
+    { name: "animated rotateInDownRight", level: 2 },
+    { name: "animated rotateInUpRight", level: 2 },
+    { name: "animated rotateInUpLeft", level: 2 },
+
+];
+
+
+var outAnimationsrep = [
+    { name: "shrink", level: 2 /* Standard */ },
+    { name: "background-fade-out", level: 1 /* Basic */ },
+    { name: "rotatex-out", level: 2 /* Standard */ },
+    { name: "rotateY-out", level: 2 /* Standard */ },
+    //animate.css
+    { name: "animated bounceOut", level: 1 },
+    { name: "animated bounceOutUp", level: 1 },
+    { name: "animated bounceOutDown", level: 1 },
+    { name: "animated bounceOutRight", level: 1 },
+    { name: "animated bounceOutLeft", level: 1 },
+    { name: "animated slideOutUp", level: 1 },
+    { name: "animated slideOutDown", level: 1 },
+    { name: "animated slideOutRight", level: 1 },
+    { name: "animated slideOutLeft", level: 1 },
+    { name: "animated lightSpeedOut", level: 2 },
+    { name: "animated zoomOutLeft", level: 2 },
+    { name: "animated zoomOutRight", level: 2 },
+    { name: "animated zoomOutUp", level: 2 },
+    { name: "animated zoomOutDown", level: 2 },
+    { name: "animated zoomOut", level: 2 },
+    { name: "animated rollOut", level: 2 },
+    { name: "animated lightSpeedOut", level: 2 },
+    { name: "animated fadeOut", level: 2 },
+    { name: "animated fadeOutDownBig", level: 2 },
+    { name: "animated fadeOutLeftBig", level: 2 },
+    { name: "animated fadeOutRightBig", level: 2 },
+    { name: "animated fadeOutUpBig", level: 2 },
+    { name: "animated fadeOutDownBig", level: 2 },
+    { name: "animated flipOutX", level: 2 },
+    { name: "animated flipOutY", level: 2 },
+    { name: "animated rotateOutDownLeft", level: 2 },
+    { name: "animated rotateOutDownRight", level: 2 },
+    { name: "animated rotateOutUpRight", level: 2 },
+    { name: "animated rotateOutUpLeft", level: 2 },
+    { name: "animated hinge", level: 2 },
+];
+
+
+var coldAnimationsrep = [
+    { name: "gray-in", level: 1 /* Basic */ },
+    { name: "sepia-in", level: 1 /* Basic */ },
+    { name: "contrast-in", level: 2 /* All */ },
+    { name: "hue-rotate-in", level: 2 /* All */ },
+    { name: "invert-in", level: 2 /* All */ },
+    { name: "blur-in", level: 2 /* All */ },
+    { name: "saturate-in", level: 2 /* All */ },
+    //animate.css
+     { name: "animated wobble", level: 3 },
+     { name: "animated flip", level: 3 },
+     { name: "animated swing", level: 3 },
+     { name: "animated rubberBand", level: 3 },
+     { name: "animated flash", level: 3 },
+     { name: "animated pulse", level: 3 },
+     { name: "animated shake", level: 3 },
+     { name: "animated tada", level: 3 },
+     { name: "animated jello", level: 3 },
+
+];
+
+
+var coldStatesrep = [
+    { name: "grayscale", level: 1 /* Basic */ },
+    { name: "sepia", level: 1 /* Basic */ },
+    { name: "contrast", level: 2 /* All */ },
+    { name: "hue-rotate", level: 2 /* All */ },
+    { name: "invert", level: 2 /* All */ },
+    { name: "blur", level: 2 /* All */ },
+    { name: "saturate", level: 2 /* All */ }
+];
+
+
+var textStylesrep = [
+    { name: "rainbow", level: 2 /* All */ },
+    { name: "retro", level: 2 /* All */ },
+    { name: "text-style1", level: 1 /* Basic */ },
+    { name: "inset", level: 1 /* Basic */ },
+    { name: "tri-dimension", level: 1 /* Basic */ },
+    //no style
+    //{ name: "tri-dimension", level: 0 /* Basic */ },
+    //{ name: "tri-dimension", level: 0 /* Basic */ },
+    //{ name: "tri-dimension", level: 0 /* Basic */ },
+    //{ name: "tri-dimension", level: 0 /* Basic */ },
+];
+
+
+
+var textAnimesrep = [
+    { name: "neon-glow", level: 1 /* Basic */ },
+    { name: "spin-around", level: 1 /* Basic */ },
+    { name: "space-in-out", level: 1 /* Basic */ },
+    { name: "pass-by", level: 2 /* All */ },
+    { name: "skew-in", level: 2 /* All */ },
+    //animate.css
+    { name: "animated bounceInRight", level: 1 },
+    { name: "animated slideInUp", level: 1 },
+    { name: "animated rotateInDownLeft", level: 2 },
+    { name: "animated lightSpeedIn", level: 2 },
+    { name: "animated fadeInUp", level: 2 },
+    { name: "animated zoomInLeft", level: 2 },
+    { name: "animated rollIn", level: 2 },
+    { name: "animated wobble", level: 2 },
+     { name: "animated flip", level: 2 },
+     { name: "animated swing", level: 2 },
+     { name: "animated rubberBand", level: 2 },
+     { name: "animated flash", level: 2 },
+     { name: "animated pulse", level: 2 },
+     { name: "animated shake", level: 2 },
+     { name: "animated tada", level: 2 },
+     { name: "animated jello", level: 2 },
+];
