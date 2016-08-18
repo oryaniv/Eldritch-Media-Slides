ff8e7c18eb56687b3de0f630a467d532c83b4e7f
Or Yaniv <oryaniv@users.noreply.github.com>
2016-08-18 23:42:23 +0300

    Update landing.js

--- landing.js
+++ landing.js
@@ -14,7 +14,7 @@ var slides3 = [0,1,2,4].map(function(e){return {path:bpath+"img"+e+".jpg",lifeti
 var slides4 = [16,17,18,19].map(function(e){return {path:bpath+"img"+e+".jpg",lifetime:2}});
 
 var sd1 = EldritchSlideShow.Create("#one",{Slides:slides1,Poster:bpath+"1.PNG",CssClass:"quarter",Animations:3,Filters:3});
-var sd2 = EldritchSlideShow.Create("#two",{Slides:slides2,Poster:bpath+"2.PNG",CssClass:"quarter",Animations:3,backgroundbetween:true,Backgrounds:["eldritch.jpg"]});
+var sd2 = EldritchSlideShow.Create("#two",{Slides:slides2,Poster:bpath+"2.PNG",CssClass:"quarter",Animations:3,backgroundbetween:true,Backgrounds:[{path:"eldritch.jpg"}]});
 var sd3 = EldritchSlideShow.Create("#three",{Slides:slides3,Poster:bpath+"3.PNG",CssClass:"quarter",Animations:3,Filters:2});
 var sd4 = EldritchSlideShow.Create("#four",{Slides:slides4,Poster:bpath+"4.PNG",CssClass:"quarter",Animations:3});
 
